b7ef005fa55d8d47247ca9fd8d30b205a8b382f5
Advika Sinha <152763494+advikasinha@users.noreply.github.com>
2024-07-10 15:17:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
-
-# Install Rust
+echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Add Rust to the PATH
-source $HOME/.cargo/env
+echo "Adding Rust to PATH..."
+if [[ "$OSTYPE" == "msys" ]]; then
+    source $USERPROFILE/.cargo/env
+else
+    source $HOME/.cargo/env
+fi
+
+# Verify Rust installation
+echo "Rust version:"
+rustc --version
+cargo --version
