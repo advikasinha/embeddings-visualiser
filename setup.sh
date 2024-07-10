cd2614615c3d19fa5d817f636c4e4f6e2a3f9fa3
Advika Sinha <152763494+advikasinha@users.noreply.github.com>
2024-07-10 15:22:56 +0530

    source replaced by .

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Add Rust to the PATH
 echo "Adding Rust to PATH..."
 if [[ "$OSTYPE" == "msys" ]]; then
-    source $USERPROFILE/.cargo/env
+    . $USERPROFILE/.cargo/env
 else
-    source $HOME/.cargo/env
+    . $HOME/.cargo/env
 fi
 
 # Verify Rust installation
